403cb25998b321d06242fcbd2c3099bdf8eaffd2
Matt Butcher <mbutcher@engineyard.com>
2015-07-14 16:41:52 -0600

    fix(deisctl): exit when stop/start fails
    This fixes issue #3380, where a failed start or stop command will hang
    deisctl. The fix simply prints out an error message and exits from the
    resolution loop.

--- deisctl/utils/utils_test.go
+++ deisctl/utils/utils_test.go
@@ -1,9 +1,7 @@
 package utils
 
 import (
-	"fmt"
 	"os"
-	"strings"
 	"testing"
 )
 
@@ -18,28 +16,5 @@ func TestResolvePath(t *testing.T) {
 		if resolved != expected {
 			t.Errorf("Test failed: expected %s, got %s", expected, resolved)
 		}
-	}
-}
-
-func TestColorize(t *testing.T) {
-	out := Colorize("{{.Red}}Hello {{.Default}}World{{.UnderGreen}}!{{.Default}}")
-	expected := "\033[0;31mHello \033[0mWorld\033[4;32m!\033[0m"
-	if out != expected {
-		t.Errorf("Expected '%s', got '%s'", expected, out)
-	}
-}
-
-func ExampleColorize() {
-	out := Colorize("{{.Red}}Hello {{.Default}}World{{.UnderGreen}}!{{.Default}}")
-	fmt.Println(out)
-}
-
-func TestDeisIfy(t *testing.T) {
-	d := DeisIfy("Test")
-	if strings.Contains(d, "Deis1") {
-		t.Errorf("Failed to compile template")
-	}
-	if !strings.Contains(d, "Test") {
-		t.Errorf("Failed to render template")
 	}
 }
